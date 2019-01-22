37ac005c769b6362ca08a8a9d8648c8a6649ced8
seppy87 <33813774+seppy87@users.noreply.github.com>
2019-01-22 16:00:47 +0100

    yesterday's work

--- src/workers.cpp
+++ src/workers.cpp
@@ -73,26 +73,109 @@ void NeXt::run() {
 	using Poco::BasicEvent;
 	using Poco::Delegate;
 	modbus_t* mb;
-	mb = modbus_new_tcp(config::config::ip.c_str(), config::config::port);
+	mb = modbus_new_tcp("127.0.0.1", 1502);
 	this->crTarget = new target::SendConsumptionRequestTarget(mb);
 	this->prResult.theEvent += Poco::delegate(this->crTarget, &target::SendConsumptionRequestTarget::onEvent);
-	modbus_connect(mb);
-	uint16_t reg[32];
-	while (this->ErrorAbort == false && this->quitRequest == false) {
-		int rc = modbus_read_registers(mb, 0, 10, reg);
-		if (rc < 0) {
-			std::cout << "ERROR OCCURED" << std::endl;
+	int s = -1;
+	modbus_mapping_t *mb_mapping;
+	int rc, i, header_length;
+	uint8_t* query;
+	query = (uint8_t*)malloc(MODBUS_MAX_ADU_LENGTH);
+	header_length = modbus_get_header_length(mb);
+	mb_mapping = modbus_mapping_new_start_address(
+		UT_BITS_ADDRESS, UT_BITS_NB,
+		UT_INPUT_BITS_ADDRESS, UT_INPUT_BITS_NB,
+		UT_REGISTERS_ADDRESS, UT_REGISTERS_NB_MAX,
+		UT_INPUT_REGISTERS_ADDRESS, UT_INPUT_REGISTERS_NB);
+	if (mb_mapping == NULL) {
+		fprintf(stderr, "Failed to allocate the mapping: %s\n",
+			modbus_strerror(errno));
+		modbus_free(mb);
+		return;
+	}
+	modbus_set_bits_from_bytes(mb_mapping->tab_input_bits, 0, UT_INPUT_BITS_NB,
+		UT_INPUT_BITS_TAB);
+	for (i = 0; i < UT_INPUT_REGISTERS_NB; i++) {
+		mb_mapping->tab_input_registers[i] = UT_INPUT_REGISTERS_TAB[i];;
+	}
+	s = modbus_tcp_listen(mb, 1);
+	modbus_tcp_accept(mb, &s);
+	for (;;) {
+		do {
+			rc = modbus_receive(mb, query);
+			std::cout << "RC=" << rc << std::endl; //DEBUG
+		} while (rc == 0);
+		if (rc == -1 && errno != EMBBADCRC) break;
+		if(query[header_length] == 0x03){
+			/* Read holding registers */
+
+			if (MODBUS_GET_INT16_FROM_INT8(query, header_length + 3)
+				== UT_REGISTERS_NB_SPECIAL) {
+				printf("Set an incorrect number of values\n");
+				MODBUS_SET_INT16_TO_INT8(query, header_length + 3,
+					UT_REGISTERS_NB_SPECIAL - 1);
+			}
+			else if (MODBUS_GET_INT16_FROM_INT8(query, header_length + 1)
+				== UT_REGISTERS_ADDRESS_SPECIAL) {
+				printf("Reply to this special register address by an exception\n");
+				modbus_reply_exception(mb, query,
+					MODBUS_EXCEPTION_SLAVE_OR_SERVER_BUSY);
+				continue;
+			}
+			else if (MODBUS_GET_INT16_FROM_INT8(query, header_length + 1)
+				== UT_REGISTERS_ADDRESS_INVALID_TID_OR_SLAVE) {
+				const int RAW_REQ_LENGTH = 5;
+				uint8_t raw_req[] = {
+					0xFF,
+					0x03,
+					0x02, 0x00, 0x00
+				};
+
+				printf("Reply with an invalid TID or slave\n");
+				modbus_send_raw_request(ctx, raw_req, RAW_REQ_LENGTH * sizeof(uint8_t));
+				continue;
+			}
+			else if (MODBUS_GET_INT16_FROM_INT8(query, header_length + 1)
+				== UT_REGISTERS_ADDRESS_SLEEP_500_MS) {
+				printf("Sleep 0.5 s before replying\n");
+				Sleep(500);
+			}
+			else if (MODBUS_GET_INT16_FROM_INT8(query, header_length + 1)
+				== UT_REGISTERS_ADDRESS_BYTE_SLEEP_5_MS) {
+				/* Test low level only available in TCP mode */
+				/* Catch the reply and send reply byte a byte */
+				uint8_t req[] = "\x00\x1C\x00\x00\x00\x05\xFF\x03\x02\x00\x00";
+				int req_length = 11;
+				int w_s = modbus_get_socket(mb);
+				if (w_s == -1) {
+					fprintf(stderr, "Unable to get a valid socket in special test\n");
+					continue;
+				}
+
+				/* Copy TID */
+				req[1] = query[1];
+				for (i = 0; i < req_length; i++) {
+					printf("(%.2X)", req[i]);
+					Sleep(500);
+					rc = send(w_s, (const char*)(req + i), 1, 0);
+					if (rc == -1) {
+						break;
+					}
+				}
+				continue;
+			}
+		}
+
+		rc = modbus_reply(mb, query, rc, mb_mapping);
+		if (rc == -1) {
 			break;
 		}
-		if (reg[12] == 1) {
-			// read input
-			events::EvArgs args;
-			args.target = mb;
-			this->prResult.InputReceived(args);
-		}
-		Sleep(2000);
 	}
+	modbus_mapping_free(mb_mapping);
+	free(query);
+	/* For RTU */
 	modbus_close(mb);
+	modbus_free(mb);
 }
 
 void NeXt::receiveMessage(const void* pSender, std::string& msg) {
